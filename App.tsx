02e5361fa96d867236d0e38010c89aaf60279c72
Liam Perritt <liam.perritt@reece.com.au>
2025-05-18 17:45:11 +1000

    Open external links in browser and disable notifications for now

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { View, SafeAreaView, ActivityIndicator, StyleSheet, BackHandler, Text, Platform } from "react-native";
+import { View, SafeAreaView, ActivityIndicator, StyleSheet, BackHandler, Text, Platform, Linking } from "react-native";
 import WebView from "react-native-webview";
 import BackgroundFetch from "react-native-background-fetch";
 import PushNotification from 'react-native-push-notification';
@@ -17,6 +17,7 @@ const DEFAULT_FILTERS = [
   ".xh8yej3.x1n2onr6.xaw8158.x1q0g3np.x78zum5.x9f619.x178xt8z.x13fuv20.x1yvgwvq.xaeubzz.x1o5hw5a > .xh8yej3.xaw8158.x78zum5 > div:nth-of-type(3)", // Nav Reels icon
   ".x1hc1fzr.x51ohtg.x2em05j.xqu0tyb.xi2lk0m.x19991ni.x1g2r6go.x10l6tqk.xww2gxu.x18nykt9.xudhj91.x14yjl9h.x14vhib7", // Nav Profile icon red dot
   "._abpk._acc8", // "Use the app" popup
+  ".xp4054r.x1q0g3np.x78zum5.x6s0dn4 > .x1nhvcw1.x1oa3qoh.x1qjc9v5.xqjyukv.xdt5ytf.x2lah0s.x1c4vz4f.xryxfnj.x1plvlek.x1uhb9sk.xo71vjh.x5pf9jr.x13lgxp2.x168nmei.x78zum5.xjbqb8w.x9f619", // Open App button
   // Profile
   ".x1vjfegm.x1a2a7pz.x1lku1pv.x87ps6o.x1q0g3np.x3nfvp2.x13rtm0m.x1e5q0jg.x3x9cwd.x1o1ewxj.x1t137rt.xggy1nq.x1hl2dhg.x16tdsg8.x1n2onr6.xkhd6sd.x18d9i69.x4uap5.xexx8yu.xeuugli.x2lwn1j.x1mh8g0r.xat24cr.x11i5rnm.xdj266r.xe8uvvx.x2lah0s.xdl72j9.x1ypdohk.x9f619.xm0m39n.x1qhh985.xcfux6l.x972fbf.x26u7qi.x1q0q8m5.xu3j5b3.x13fuv20.x2hbi6w.xqeqjp1.xa49m3k.xjqpnuy.xjbqb8w.x1qjc9v5.x1i10hfl", // Profile Note bubble
   "._ab1b._ab18 > .x1qrby5j.x7ja8zs.x1t2pt76.x1lytzrv.xedcshv.xarpa2k.x3igimt.x12ejxvf.xaigb6o.x1beo9mf.xv2umb2.x1jfb8zj.x1h9r5lt.x1h91t0o.x4k7w5x", // Threads icon
@@ -46,12 +47,13 @@ PushNotification.configure({
     sound: true,
   },
   popInitialNotification: true,
-  requestPermissions: Platform.OS === 'ios',
+  requestPermissions: false, // Platform.OS === 'ios', // Uncomment this line to request permissions on iOS
 });
 
 const App = () => {
   const webViewRef = useRef<WebView>(null);
-  const baseUrl = `https://www.instagram.com/`;
+  const baseUrlShort = `instagram.com`;
+  const baseUrl = `https://www.${baseUrlShort}/`;
   const sourceUrl = `${baseUrl}direct/inbox/`;
   const apiUrl = `https://i.instagram.com/api/v1/direct_v2/inbox/`;
   const redirectFromUrls = [
@@ -59,6 +61,10 @@ const App = () => {
     `${baseUrl}reels/`,
     `${baseUrl}notifications/`,
   ];
+  const openableExternalUrls = [
+    "https://www.facebook.com/instagram/",
+    "https://www.fbsbx.com/",
+  ]
   const configUrl = "https://raw.githubusercontent.com/liamperritt/social-minimalist-config/refs/heads/main/config/instagram/";
 
   const [currentUrl, setCurrentUrl] = useState("");
@@ -359,8 +365,8 @@ const App = () => {
     // Only hit Instagram's internal API if the user has granted notification permission
     console.log("Checking push notification permissions...");
     PushNotification.checkPermissions(async (permissions: any) => {
-      if (!permissions.alert) {
-        console.log("Push notification permissions denied");
+      if (true) { // if (!permissions.alert) { // TODO: Uncomment these lines once we're ready to enable notifications
+        // console.log("Push notification permissions denied");
         return callback();
       }
       console.log("Push notification permissions granted");
@@ -436,22 +442,32 @@ const App = () => {
     setHasLoadError(false);
   };
 
+  const handleShouldStartLoadWithRequest = (request: any) => {
+    if (!request.url.includes(baseUrlShort) && !openableExternalUrls.some(url => request.url.startsWith(url))) {
+      console.log("External link detected, opening in default browser:", request.url);
+      // Open external links in the device's default browser
+      Linking.openURL(request.url);
+      return false;
+    }
+    return true; // Allow the WebView to load the URL
+  }
+
   const handleNavigationStateChange = (navState: any) => {
     console.log("Handling navigation state change:", navState);
     if (!webViewRef.current) return;
     redirectToSafety(navState);
     if (navState.url === sourceUrl) {
-      // Get cookies and user agent from the WebView
-      console.log("Injecting JavaScript to get cookies and user agent...");
-      webViewRef.current.injectJavaScript(
-        `(function() {
-          const userAgent = navigator.userAgent;
-          window.ReactNativeWebView.postMessage(JSON.stringify({ userAgent }));
-        })();
-        true;`
-      );
-      console.log("Fetching unread messages...");
-      notifyOfUnreadMessageIfPermitted(false);
+      // TODO: Uncomment to get cookies and user agent from the WebView
+      // console.log("Injecting JavaScript to get cookies and user agent...");
+      // webViewRef.current.injectJavaScript(
+      //   `(function() {
+      //     const userAgent = navigator.userAgent;
+      //     window.ReactNativeWebView.postMessage(JSON.stringify({ userAgent }));
+      //   })();
+      //   true;`
+      // );
+      // console.log("Fetching unread messages...");
+      // notifyOfUnreadMessageIfPermitted(false);
     }
   };
 
@@ -489,6 +505,7 @@ const App = () => {
         onError={() => {handleLoadError()}}
         onLoad={(syntheticEvent) => {handleLoadSuccess(syntheticEvent.nativeEvent)}}
         onLoadStart={(syntheticEvent) => {trackNavState(syntheticEvent.nativeEvent)}}
+        onShouldStartLoadWithRequest={handleShouldStartLoadWithRequest}
         onNavigationStateChange={handleNavigationStateChange}
         onOpenWindow={(syntheticEvent) => {openLinkInWebView(syntheticEvent.nativeEvent)}}
         onContentProcessDidTerminate={handleProcessTermination}
